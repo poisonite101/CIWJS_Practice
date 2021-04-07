a403f96cffe7492078a9c52f8da91b518cb9d95e
Zac <zacace@ingoglia.com>
2021-04-07 15:28:02 -0700

    Day 5

--- ladder.js
+++ ladder.js
@@ -1,17 +1,32 @@
 let ladder = {
   position: 0,
+  height: 0,
   up() {
-    this.position += 1;
+    if (this.position < this.height) {
+      this.position += 1;
+    } else {
+      console.log(`Max Height Reached!`);
+    }
+    return;
   },
   down() {
-    this.position -= 1;
+    if (this.position > 0) {
+      this.position -= 1;
+    } else {
+      console.log(`Min Height Reached!`);
+    }
+    return;
   },
   showStep() {
     return this.position;
   },
 };
 
+ladder.height = 5;
 ladder.up();
 ladder.up();
 ladder.down();
-alert(`Current step is: ${ladder.showStep()}`);
+ladder.up();
+ladder.down();
+// alert(`Current step is: ${ladder.showStep()}`);
+console.log(`Current step is: ${ladder.showStep()}`);
